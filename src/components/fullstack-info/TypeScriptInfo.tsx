3c2fd031210a0d898167e9f6e4c21da3b17dc7a4
Константин <konstantin.larin.33@gmail.com>
2024-10-17 17:07:09 +0300

    fix ts

--- src/components/fullstack-info/TypeScriptInfo.tsx
+++ src/components/fullstack-info/TypeScriptInfo.tsx
@@ -5,8 +5,8 @@ export default function TypeScriptInfo(){
     const {language} = useContext(LanguageContext);
     return (
         <div className={'block-info-inner'}>
-            <p>{fullstackContenxt.TS_1[language]}.</p>
-            <p>{fullstackContenxt.TS_2[language]}.</p>
+            <p>{fullstackContent.TS_1[language]}.</p>
+            <p>{fullstackContent.TS_2[language]}.</p>
         </div>
     )
 }
